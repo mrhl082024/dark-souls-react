488568a6f7df15725e4422d1dc79e0bfd57e0250
mrhl082024 <mrhl082024@gmail.com>
2025-02-14 10:37:34 +0100

    added buttons and grid, added colors, changed size and sutff

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -4,23 +4,46 @@ const NavBar = () => {
   return (
     <div id="navbar-card">
       <nav id="navbar">
-        <div class="dropdown">
-          <button class="dropbtn">Magic</button>
-          <div class="dropdown-content">
+        <div className="dropdown">
+          <button className="dropbtn">World</button>
+          <div className="dropdown-content">
+            <a href="">Placeholder</a>
+            <a href="">Placeholder</a>
+            <a href="">Placeholder</a>
+          </div>
+        </div>
+        <div className="dropdown">
+          <button className="dropbtn">Placeholder</button>
+          <div className="dropdown-content">
+            <a href="">Placeholder</a>
+            <a href="">Placeholder</a>
+            <a href="">Placeholder</a>
+          </div>
+        </div>
+        <div className="dropdown">
+          <button className="dropbtn">Equipment</button>
+          <div className="dropdown-content">
+            <a href="">Armour</a>
+            <a href="">Weapons</a>
+            <a href="">Catalysts</a>
+          </div>
+        </div>
+        <div className="dropdown">
+          <button className="dropbtn">Magic</button>
+          <div className="dropdown-content">
             <a href="">Sorcery</a>
             <a href="">Miracles</a>
             <a href="">Pyromancy</a>
           </div>
         </div>
-        <div class="dropdown">
-          <button class="dropbtn">Magic</button>
-          <div class="dropdown-content">
-            <a href="">Sorcery</a>
-            <a href="">Miracles</a>
-            <a href="">Pyromancy</a>
+        <div className="dropdown">
+          <button className="dropbtn">Items</button>
+          <div className="dropdown-content">
+            <a href="">Placeholder</a>
+            <a href="">Placeholder</a>
+            <a href="">Placeholder</a>
           </div>
         </div>
-        
       </nav>
     </div>
   );
