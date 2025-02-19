2db19ffc2d706076996fb2745134c1e05f886903
mrhl082024 <mrhl082024@gmail.com>
2025-02-19 12:21:46 +0100

    done grid

--- src/component/SorceryList.jsx
+++ src/component/SorceryList.jsx
@@ -1,5 +1,5 @@
 import magicData from "../data/magic.json";
-import "../styles/SorceryList.css";
+import "../styles/MagicList.css";
 
 const SorceryList = () => {
   //JS code goes here--
@@ -8,18 +8,26 @@ const SorceryList = () => {
     //HTML code goes here--
     <>
       {magicData.sorcery.map((spell, i) => (
-        <div key={i} id="card">
-          <h2> {spell.name} </h2>
-          <div id="img-container">
-            <img id="img" src={spell.img} alt="Picture of a sorcery spell." />
-          </div>
-          <p id="description">{spell.description} </p>
-          <ul id="stat-table">
-            <li id="fp">FP cost: {spell.fp} </li>
-            <li id="slots">Slots: {spell.slots} </li>
-            <li id="intelligence">Int req: {spell.intelligence} </li>
-            <li id="faith">Fth req: {spell.faith} </li>
-          </ul>
+        <div key={i} id="sorcery-card">
+          <section id="name-img-container">
+            <h2 id="name"> {spell.name} </h2>
+            <div id="img-container">
+              <img id="img" src={spell.img} alt="Picture of a sorcery spell." />
+            </div>
+          </section>
+          <section id="description-container">
+            <h2 id="description-title">Description</h2>
+            <p id="description">{spell.description} </p>
+          </section>
+          <section id="stats-container">
+            <h2 id="stats-title">Stats</h2>
+            <ul id="stats-table">
+              <li id="fp">FP cost: {spell.fp} </li>
+              <li id="slots">Slots: {spell.slots} </li>
+              <li id="intelligence">Int req: {spell.intelligence} </li>
+              <li id="faith">Fth req: {spell.faith} </li>
+            </ul>
+          </section>
         </div>
       ))}
     </>
