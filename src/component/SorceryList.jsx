c541eef40a4e29538a39f477f1b6f2f1063e3193
mrhl082024 <mrhl082024@gmail.com>
2025-02-19 11:40:22 +0100

    made template for sorcerylist, added pinctures and changed some magic.json things

--- src/component/SorceryList.jsx
+++ src/component/SorceryList.jsx
@@ -1,24 +1,25 @@
 import magicData from "../data/magic.json";
 import "../styles/SorceryList.css";
 
-// console.log(magicData[0].sorcery);
-// Object.keys(magicData[0].sorcery).map((key) =>
-//   console.log(magicData[0].sorcery[key])
-// );
-
 const SorceryList = () => {
   //JS code goes here--
 
   return (
     //HTML code goes here--
-
     <>
       {magicData.sorcery.map((spell, i) => (
         <div key={i} id="card">
-          <p> {spell.name} </p>
+          <h2> {spell.name} </h2>
           <div id="img-container">
             <img id="img" src={spell.img} alt="Picture of a sorcery spell." />
           </div>
+          <p id="description">{spell.description} </p>
+          <ul id="stat-table">
+            <li id="fp">FP cost: {spell.fp} </li>
+            <li id="slots">Slots: {spell.slots} </li>
+            <li id="intelligence">Int req: {spell.intelligence} </li>
+            <li id="faith">Fth req: {spell.faith} </li>
+          </ul>
         </div>
       ))}
     </>
