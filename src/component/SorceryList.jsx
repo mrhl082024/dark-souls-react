a289c77f7382db216b750f6ec1866f525a2d8d06
mrhl082024 <mrhl082024@gmail.com>
2025-02-24 14:49:19 +0100

    bong

--- src/component/SorceryList.jsx
+++ src/component/SorceryList.jsx
@@ -10,35 +10,33 @@ const SorceryList = () => {
     //HTML code goes here--
     <>
       {magicData.sorcery.map((spell, i) => (
-        <div key={i} id="magic-card">
-          <section id="name-img-container">
-            <Link id="name" to="/MagicWindow">
+        <Link id="name" to="/MagicWindow">
+          <div key={i} id="magic-card">
+            <section id="name-img-container">
               <h2> {spell.name} </h2>
-            </Link>
-            <div id="img-container">
-              <Link to="/MagicWindow">
+              <div id="img-container">
                 <img
                   id="img"
                   src={spell.img}
                   alt="Picture of a sorcery spell."
                 />
-              </Link>
-            </div>
-          </section>
-          <section id="description-container">
-            <h2 id="description-title">Description</h2>
-            <p id="description">{spell.description} </p>
-          </section>
-          <section id="stats-container">
-            <h2 id="stats-title">Stats</h2>
-            <ul id="stats-table">
-              <li id="fp">FP cost: {spell.fp} </li>
-              <li id="slots">Slots: {spell.slots} </li>
-              <li id="intelligence">Int req: {spell.intelligence} </li>
-              <li id="faith">Fth req: {spell.faith} </li>
-            </ul>
-          </section>
-        </div>
+              </div>
+            </section>
+            <section id="description-container">
+              <h2 id="description-title">Description</h2>
+              <p id="description">{spell.description} </p>
+            </section>
+            <section id="stats-container">
+              <h2 id="stats-title">Stats</h2>
+              <ul id="stats-table">
+                <li id="fp">FP cost: {spell.fp} </li>
+                <li id="slots">Slots: {spell.slots} </li>
+                <li id="intelligence">Int req: {spell.intelligence} </li>
+                <li id="faith">Fth req: {spell.faith} </li>
+              </ul>
+            </section>
+          </div>
+        </Link>
       ))}
     </>
   );
